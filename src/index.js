e88f19b58c8f04e9f71ad7192358ab51d1e1f769
blackdanamite <seagrendaniel562@gmail.com>
2019-11-11 14:35:36 -0800

    add tie functionality

--- src/index.js
+++ src/index.js
@@ -106,22 +106,25 @@ class Game extends React.Component {
       let status;
       if (winner) {
         status = "Winner: " + winner;
+      } else if(!winner && this.state.stepNumber === 9) {
+        status = `It's a tie!`;
       } else {
         status = "Next player: " + (this.state.xIsNext ? "X" : "O");
       }
   
       return (
         <div className="game">
+          <div className="game-info">
+            <h1>React Tic Tac Toe</h1>
+            <div>{status}</div>
+          </div>
           <div className="game-board">
             <Board
               squares={current.squares}
               onClick={i => this.handleClick(i)}
-            />
+              />
           </div>
-          <div className="game-info">
-            <div>{status}</div>
-            <ol>{moves}</ol>
-          </div>
+              <ol className="moves-list">{moves}</ol>
         </div>
       );
     }
